f9a95739abe3fd29fc9a0d21eba5e27cef268a3d
Petros Tsavdaridis <peteropon@gmail.com>
2021-06-25 14:40:18 +0200

    Added create lambda.

---
+++ create.js
@@ -0,0 +1,24 @@
+import * as uuid from "uuid";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      // The attributes of the item to be created
+      guitarId: uuid.v1(), // A unique uuid
+      username: "fdasadfda",
+      description: data.description, // Parsed from request body
+      attachment: data.attachment, // Parsed from request body
+      votes: 0,
+      comments: [],
+      createdAt: Date.now(), // Current Unix timestamp
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return params.Item;
+});
