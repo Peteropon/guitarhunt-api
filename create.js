6310635246075b8103058e2efb3f7a5c0d5c8b9f
Petros Tsavdaridis <peteropon@gmail.com>
2021-06-27 23:22:50 +0200

    Added two fields in the create func.

--- create.js
+++ create.js
@@ -10,10 +10,12 @@ export const main = handler(async (event, context) => {
       // The attributes of the item to be created
       guitarId: uuid.v1(), // A unique uuid
       username: "fdasadfda",
+      title: data.title,
       description: data.description, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
       votes: 0,
       comments: [],
+      isActive: true,
       createdAt: Date.now(), // Current Unix timestamp
     },
   };
